7df1eeed3583ff048ec945dc16ff82f0f4a91443
TheoMoulart <tmoulart@oslo.fr>
2023-02-20 15:13:57 +0100

    feat(LoaderComponent.vue): adding loader component with cocktail animation

---
+++ src/components/LoaderComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col items-center">
+    <Vue3Lottie :animationData="LemonadeJSON" :height="500" :width="400" />
+    <div class="text-3xl">
+      Préparation des cocktails...
+    </div>
+  </div>
+</template>
+
+<script>
+import { Vue3Lottie } from 'vue3-lottie'
+import 'vue3-lottie/dist/style.css'
+import LemonadeJSON from '../assets/lemonade.json';
+export default {
+  components: {
+    Vue3Lottie
+  },
+  data() {
+    return {
+      LemonadeJSON,
+    }
+  },
+}
+</script>
